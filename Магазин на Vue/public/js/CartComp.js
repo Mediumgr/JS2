34072dd78904577ed3a89182efd57ed42865f7e4
Руслан <Mediumgr@yandex.ru>
2021-06-01 00:39:54 +0300

    StoreOnVue
    Моя работа в процессе

---
+++ Магазин на Vue/public/js/CartComp.js
@@ -0,0 +1,142 @@
+Vue.component('cart', {
+    data() {
+        return {
+            /*       cartUrl: '/getBasket.json', */
+            cartItems: [],
+            showCart: false,
+            totalPrice: ''
+        }
+    },
+    methods: {
+        addProduct(product) {
+            let find = this.cartItems.find(el => el.id_product === product.id_product);
+            if (find) {
+                this.$parent.putJson(`/api/cart/${find.id_product}`, {
+                    quantity: 1
+                });
+                find.quantity++;
+            } else {
+                let prod = Object.assign({
+                    quantity: 1
+                }, product);
+                this.$parent.postJson('/api/cart', prod)
+                    .then(data => {
+                        if (data.result === 1) {
+                            this.cartItems.push(prod);
+                            this.productsPriceShow();
+                        }
+                    });
+            }
+            this.productsPriceShow();
+        },
+        remove(item) {
+            if (item.quantity > 1) {
+                this.$parent.putJson(`/api/cart/${item.id_product}`, {
+                        quantity: -1
+                    })
+                    .then(data => {
+                        if (data.result === 1) {
+                            item.quantity--;
+                        }
+                    });
+            } else {
+                this.$parent.deleteJson(`/api/cart/${item.id_product}`)
+                    .then(data => {
+                        if (data.result === 1) {
+                            this.cartItems.splice(this.cartItems.indexOf(item), 1);
+                            this.productsPriceShow();
+                        }
+                    });
+            }
+            this.productsPriceShow();
+        },
+        productsPriceShow() {
+            this.outputPrice = this.calcSum();
+            this.totalPrice = 'Итого: ' + `${this.outputPrice}` + ' $';
+        },
+        calcSum() {
+            return this.cartItems.reduce((accum, item) => accum + (item.price * item.quantity), 0);
+        },
+    },
+    mounted() {
+        this.$parent.getJson('/api/cart')
+            .then(data => {
+                for (let el of data.contents) {
+                    this.cartItems.push(el);
+                    this.productsPriceShow();
+                }
+            });
+    },
+    template:
+        /* `
+               <div>
+                   <button class="btn-cart" type="button" @click="showCart = !showCart">Корзина</button>
+                   <div class="cart-block" v-show="showCart">
+                      <p v-if="!cartItems.length" style="padding-left: 55px">В корзине нет товаров</p>
+                       <cart-item class="cart-item" v-for="item of cartItems" :key="item.id_product":cart-item="item" :img="imgCart" @remove="remove"></cart-item>
+                    <h3 v-if=cartItems.length style="padding-left: 65px">{{totalPrice}}</h3>
+                   </div>
+               </div>` */
+
+        `     <div class="bin__for__order">
+                    <div href="#" class="bin__img">
+                        <img class="header__cart" src="img/cart.png" alt="cart"  @click="showCart=!showCart">
+                    </div>
+                    <div v-show="showCart">
+                        <p v-if="!cartItems.length" style="padding-left: 55px">В корзине нет товаров</p>
+                        <cart-item class="cart-item product__order" v-for="item of cartItems" :key="item.id_product" :cart-item="item" :img="$root.$refs.products.img" @remove="remove">
+                        </cart-item>
+                    <div class="dollars" v-if=cartItems.length>'$'{{totalPrice}}</div>
+                    </div>
+              </div>`
+});
+
+Vue.component('cart-item', {
+    props: ['cartItem', 'img'],
+    template:
+        /*  `
+                    <div class="cart-item">
+                        <div class="product-bio">
+                            <img :src="img" alt="Some image">
+                            <div class="product-desc">
+                                <p class="product-title">{{cartItem.product_name}}</p>
+                                <p class="product-quantity">Количество: {{cartItem.quantity}}</p>
+                                <p class="product-single-price">{{cartItem.price}}₽ за единицу</p>
+                            </div>
+                        </div>
+                        <div class="right-block">
+                            <p class="product-price">{{cartItem.quantity*cartItem.price}}₽</p>
+                            <button class="del-btn" @click="$emit('remove', cartItem)">&times;</button>
+                        </div>
+                    </div>
+            ` */
+
+        `   <div>
+                        <div class="order__first">
+                            <div class="rebox__img">
+                                <img :src="img" alt="your product" class="rebox__img_photo">
+                            </div>
+                            <div class="flex__name__product">
+                                <a href="#" class="rebox__text">{{cartItem.product_name}}</a>
+                                <div class="star"><img src="img/4star.jpg" alt="stars"></div>
+                                <div class="price__for__order">{{cartItem.quantity}} X {{cartItem.quantity*cartItem.price}}$</div>
+                            </div>
+                            <div class="cross" @click="$emit('remove', cartItem)">
+                                <i class="fas fa-times-circle cross-circle"></i>
+                            </div>
+                                <div class="your__total__amount">
+                                <div class="styleForTotal">Total</div>
+                        </div>
+                        </div>
+                        <div class="checkout__go">
+                            <div class="flex__button__checkout">
+                                <input type="submit" value="CHECKOUT" class="styleForCheckout">
+                            </div>
+                            <div class="flex__button__card">
+                                <input type="submit" value="GO TO CART" class="styleForCard">
+                            </div>
+                        </div>
+            </div>`
+});
+
+//   если будет ошибка, то эта строчка должна быть в cart: <div class="dollars" v-if=cartItems.length>'$'{{totalPrice}}</div>
---
+++ Магазин на Vue/public/js/ProductComp.js
@@ -0,0 +1,66 @@
+Vue.component('products', {
+    data() {
+        return {
+            /*   catalogUrl: '', */
+            products: [],
+            filtered: [],
+        }
+    },
+    methods: {
+        filter(value) {
+            let regexp = new RegExp(value, 'i');
+            this.filtered = this.products.filter(el => regexp.test(el.product_name));
+        }
+    },
+    computed: {
+        img() {
+            return `${window?.location.origin || ''}/img/teenager.png`;
+        }
+    },
+    mounted() {
+        this.$parent.getJson('/api/products')
+            .then(data => {
+                for (let el of data) {
+                    this.products.push(el);
+                    this.filtered.push(el);
+                }
+            });
+        console.log(this.img);
+    },
+    template: `
+        <div class="products center">
+            <product v-for="item of filtered" :key="item.id_product" :img="item.img" :product="item"></product>
+        </div>
+    `
+});
+Vue.component('product', {
+    props: ['product', 'img'],
+    data() {
+        return {
+            cartAPI: this.$root.$refs.cart,
+        };
+    },
+
+    template:
+        /*  `
+            <div class="product-item">
+                        <img :src="img" alt="Some img">
+                        <div class="desc">
+                            <h3>{{product.product_name}}</h3>
+                            <p>{{product.price}}₽</p>
+                            <button class="buy-btn" @click="cartAPI.addProduct(product)">Купить</button>
+        <!-- 1                    <button class="buy-btn" @click="$root.$refs.cart.addProduct(product)">Купить</button>-->
+        <!-- 2                    <button class="buy-btn" @click="$parent.$parent.$refs.cart.addProduct(product)">Купить</button>-->
+                        </div>
+                    </div>
+            ` */
+
+        `<div class="product-item">
+                <a href="#"><img class="product-item__img" :src="img" alt="photo"></a>
+                <div class="bottom">
+                    <a href="#" class="text__mango">{{product.product_name}}</a>
+                    <p class="price">{{product.price}}$</p>
+                </div>
+                <a href="#" class="add__cart" @click="cartAPI.addProduct(product)"><img src="img/white_bin.png" alt="buy">Add to Cart</a>
+        </div>`
+});
